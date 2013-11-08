f16016f694fec6f7978b39a7348cd003af577f72
Corey Dixon <Zimhey1@gmail.com>
2013-11-08 03:27:42 +0000

    Create Main.cpp

---
+++ src/Main.cpp
@@ -0,0 +1,50 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Oct 18, 2013
+ *      Author: zim
+ */
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include "robot.h"
+
+using namespace std;
+using namespace robot;
+
+int main(int argc, char* argv[])
+{
+/*	stringstream command;
+	command << "/bin/echo -e '";
+	for (int i = 1; i < argc; i++)
+	{
+		command << hex << setw(2) << setfill('0') << "\\x" << atoi(argv[i]);
+	}
+	command << '\'';
+	command << " > /dev/ttyUSB0";
+	system("stty -F /dev/ttyUSB0 57600 cs8 -cstopb");
+	system(command.str().c_str());
+	cout << endl << "Cmd: " << command.str().c_str() << endl;
+	*/
+
+
+/*
+	 unsigned int high = 0, low = 0;
+	 int num = -300;
+
+	 splitIntoTwoBytes(num, high, low);
+	 cout << "Num: " << num << endl << "High: " << high << endl << "Low: " << low << endl;
+*/
+
+	stringstream command;
+	command << "/bin/echo -e '";
+	for(int i =1; i < argc; i++)
+		command << intToHex(atoi(argv[i]));
+	command << '\'';
+	cout << command.str().c_str() << endl;
+
+	return 0;
+}
